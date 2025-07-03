7ba4d9855833c65b3171d8555102eaac15342427
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-07-03 19:16:14 +0530

    Updated flask-app port and path

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/flask-api/hello")
 def hello():
     return "Hello from Flask + Argo CD!>>>>>>>>>>>>>>>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8081)
