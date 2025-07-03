2f4b9191761a0b25106cb873763b6ca320d18544
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-07-03 22:38:15 +0530

    deployed app locallly

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/flask-api/hello")
 def hello():
-    return "Hello from Flask + Argo CD!>>>>>>>>>>>>>>><<<<<<<<"
+    return "Hello from Flask + Argo CD! >>>>>>>1235<<<<<<<<"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
