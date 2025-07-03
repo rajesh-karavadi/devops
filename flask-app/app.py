5be3ef629e16feea6e67bec6ce5dabcc86ee3f58
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-07-03 19:13:16 +0530

    Updated flask-app

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask + Argo CD!"
+    return "Hello from Flask + Argo CD!>>>>>>>>>>>>>>>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
