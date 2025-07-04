c720924a9f083afda17e75e0bb04405a078717a3
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-07-05 00:51:04 +0530

    Bumped v5 to v6

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/flask/hello")
 def hello():
-    return "Hello from Flask + Argo CD! >>>>>>>v3<<<<<<<<"
+    return "Hello from Flask + Argo CD! >>>>>>>v6<<<<<<<<"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
