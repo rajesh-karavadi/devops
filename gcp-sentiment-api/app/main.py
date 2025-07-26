0c25a50b346c6f477da130f89d65665b0314f7f5
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-07-26 20:16:35 +0530

    updated port

--- gcp-sentiment-api/app/main.py
+++ gcp-sentiment-api/app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from textblob import TextBlob
+import os
 
 app = Flask(__name__)
 
@@ -26,3 +27,5 @@ def analyze_sentiment():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
