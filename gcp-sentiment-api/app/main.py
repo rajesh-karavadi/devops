ac86ffec14147e3be6a94427e74500a360c48bee
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-07-27 23:14:17 +0530

    incremented from 1.1.1

--- gcp-sentiment-api/app/main.py
+++ gcp-sentiment-api/app/main.py
@@ -36,6 +36,5 @@ def analyze_sentiment():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
