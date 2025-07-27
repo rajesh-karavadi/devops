8ed345f5aa0c9e97a8c03bd63c209fc513a03aa9
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-07-27 23:55:36 +0530

    incremented from 1.1.5

--- gcp-sentiment-api/app/main.py
+++ gcp-sentiment-api/app/main.py
@@ -1,39 +1,40 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/flask/hello")
-def hello():
-    return "Hello from Flask ....."
+# @app.route("/flask/hello")
+# def hello():
+#     return "Hello from Flask ....."
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=8080)
-#
-# from flask import Flask, request, jsonify
-# from textblob import TextBlob
-# import os
-#
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def home():
-#     return jsonify({"message": "Welcome to Sentiment Analysis API!"})
-#
-# @app.route('/analyze', methods=['POST'])
-# def analyze_sentiment():
-#     data = request.get_json()
-#     text = data.get("text", "")
-#
-#     if not text:
-#         return jsonify({"error": "No text provided"}), 400
-#
-#     blob = TextBlob(text)
-#     sentiment = blob.sentiment
-#
-#     return jsonify({
-#         "text": text,
-#         "polarity": sentiment.polarity,
-#         "subjectivity": sentiment.subjectivity
-#     })
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
+from flask import Flask, request, jsonify
+from textblob import TextBlob
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to Sentiment Analysis API!"})
+
+@app.route('/analyze', methods=['POST'])
+def analyze_sentiment():
+    data = request.get_json()
+    text = data.get("text", "")
+
+    if not text:
+        return jsonify({"error": "No text provided"}), 400
+
+    blob = TextBlob(text)
+    sentiment = blob.sentiment
+
+    return jsonify({
+        "text": text,
+        "polarity": sentiment.polarity,
+        "subjectivity": sentiment.subjectivity
+    })
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
