2bd9a7178d7691f9da603f697a93f62020049a92
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-08-10 21:54:59 +0530

    enabled debug log

--- gcp-sentiment-api/iac/variables.tf
+++ gcp-sentiment-api/iac/variables.tf
@@ -2,8 +2,14 @@ variable "project_id" {}
 variable "region" {}
 variable "app_name" {}
 variable "image" {}
-variable "cicd_sa_email" {}
-variable "invoke_member" {}
+variable "cicd_sa_email" {
+  type = string
+  default = "terraform-cicd@peerless-robot-426711-j3.iam.gserviceaccount.com"
+}
+variable "invoke_member" {
+  type = string
+  default = "allUsers"
+}
 
 variable "environment" {
   type    = string
