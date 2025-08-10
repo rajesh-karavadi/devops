bb2fde5cfb6bbc60c9fddb3104f1a2a7c4b33f82
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-08-10 22:12:11 +0530

    role is defined

--- gcp-sentiment-api/iac/iam.tf
+++ gcp-sentiment-api/iac/iam.tf
@@ -1,18 +1,15 @@
-resource "google_project_iam_custom_role" "cicdRunDeployer" {
+
+resource "google_project_iam_custom_role" "cicd_run_deployer" {
+  project     = var.project_id
   role_id     = "cicdRunDeployer"
-  title       = "CICD Run Deployer"
-  description = "Deploys Cloud Run services"
-  permissions = [
-    "run.services.create",
-    "run.services.update",
-    # add all required permissions
-  ]
-  project = var.project_id
+  title       = "CI/CD Run Deployer"
+  description = "Custom role for deploying to Cloud Run"
+  permissions = var.role_permissions
 }
 
 resource "google_project_iam_member" "assign_cicdRunDeployer" {
   project = var.project_id
-  role    = google_project_iam_custom_role.cicdRunDeployer.name
+  role    = google_project_iam_custom_role.cicd_run_deployer.name
   member  = "serviceAccount:${var.cicd_sa_email}"
-  depends_on = [google_project_iam_custom_role.cicdRunDeployer]
+  depends_on = [google_project_iam_custom_role.cicd_run_deployer]
 }
