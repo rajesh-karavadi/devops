41e76aa64921302f9d1022d30233e675fd60b2b8
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-08-13 23:38:13 +0530

    enabled backend

--- gcp-sentiment-api/iac/main.tf
+++ gcp-sentiment-api/iac/main.tf
@@ -1,42 +1,42 @@
-# provider "google" {
-#   project = var.project_id
-#   region  = var.region
-# }
-#
-# terraform {
-#   backend "gcs" {
-#     bucket  = "my-terraform-state-bucket"  # create in GCS beforehand
-#     prefix  = "gcp-sentiment-api/state"
-#   }
-# }
-#
-# resource "google_cloud_run_service" "default" {
-#   name     = var.app_name
-#   location = var.region
-#
-#   template {
-#     spec {
-#       containers {
-#         image = var.image
-#       }
-#     }
-#   }
-#
-#   metadata {
-#     annotations = {
-#       "deployment-trigger" = var.image
-#     }
-#   }
-#
-#   traffic {
-#     percent         = 100
-#     latest_revision = true
-#   }
-#
-#   timeouts {
-#     create = "15m"
-#     update = "5m"
-#     delete = "5m"
-#   }
-# }
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "${var.project_id}-tfstate" # create in GCS beforehand
+    prefix  = "gcp-sentiment-api/state"
+  }
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = var.app_name
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = var.image
+      }
+    }
+  }
+
+  metadata {
+    annotations = {
+      "deployment-trigger" = var.image
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  timeouts {
+    create = "15m"
+    update = "5m"
+    delete = "5m"
+  }
+}
 
