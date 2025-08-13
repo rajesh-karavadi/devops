46a1847bf6dd5d3dbc154c7f854519a775d54d99
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-08-13 20:55:38 +0530

    trying bucket to store state file

---
+++ gcp-sentiment-api/iac/state-bucket.tf
@@ -0,0 +1,32 @@
+variable "location" {
+  default = "us-central1"
+}
+
+variable "bucket_name" {
+  default = "terraform-state-dev-bucket"
+}
+
+variable "sa_name" {
+  default = "terraform-cicd-dev"
+}
+
+# Create GCS bucket for remote state
+resource "google_storage_bucket" "terraform_state" {
+  name          = var.bucket_name
+  location      = var.location
+  project       = var.project_id
+  force_destroy = false
+
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+}
+
+# Grant the service account permissions on the bucket
+resource "google_storage_bucket_iam_member" "sa_bucket_access" {
+  bucket = google_storage_bucket.terraform_state.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${var.sa_name}@${var.project_id}.iam.gserviceaccount.com"
+}
