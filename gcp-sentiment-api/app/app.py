1af3a7e1e8612378fa3829c9f0e4d66ee7b5861e
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-08-14 22:14:26 +0530

    Updated port

--- gcp-sentiment-api/app/app.py
+++ gcp-sentiment-api/app/app.py
@@ -43,4 +43,4 @@ def index():
 
 if __name__ == "__main__":
     threading.Thread(target=stream_tweets, daemon=True).start()
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=8080)
